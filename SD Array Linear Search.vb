be32a0e1ce26c0222dac605125721921885765e2
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-11-25 20:14:25 +0500

    Code to search an integer value from SD array

---
+++ SD Array Linear Search.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i As Integer = 0
+        Dim Isfound As Boolean = False
+        Dim X As Integer = 0
+        Dim myArray(10) As Integer
+        Dim Y As Integer = 0
+
+        'Initialisation
+        For i = 1 To 10
+            myArray(i) = 0
+        Next
+
+        'Populating the arrays
+        For i = 1 To 10
+            Console.Write("Enter any number: ")
+            X = Console.ReadLine()
+            myArray(i) = X
+        Next
+
+        'Linear Search operation
+        Console.Write("Enter any number to be searched in the array: ")
+        Y = Console.ReadLine
+        Isfound = False
+        For i = 1 To 10
+            If myArray(i) = Y Then
+                Isfound = True
+                Exit For
+            End If
+        Next
+
+        If Isfound Then
+            Console.WriteLine("Value is found")
+        Else
+            Console.WriteLine("Value is not found")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
