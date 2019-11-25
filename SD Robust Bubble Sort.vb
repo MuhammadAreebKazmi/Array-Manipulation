1008794142f14b344c48fbfbdb4bfa3374c084fe
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-11-25 20:40:35 +0500

    It is a Robust code for bubble sort.

---
+++ SD Robust Bubble Sort.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i As Integer = 0
+        Dim j As Integer = 0
+        Dim isSorted As Boolean = True
+        Dim X As Integer = 0
+        Dim myArray(10) As Integer
+
+        'Initialisation
+        For i = 1 To 5
+            myArray(i) = 0
+        Next
+
+        'Populate
+        For i = 1 To 5
+            Console.Write("Enter the value of X: ")
+            X = Console.ReadLine
+            myArray(i) = X
+        Next
+
+        'Robust code for Bubble Sort
+        For j = 4 To 1 Step -1
+            isSorted = True
+            For i = 1 To j
+                If myArray(i) > myArray(i + 1) Then
+                    X = myArray(i)
+                    myArray(i) = myArray(i + 1)
+                    myArray(i + 1) = X
+                    isSorted = False
+                End If
+            Next
+            If isSorted = True Then
+                Exit For
+            End If
+        Next
+
+        'Output
+        For i = 1 To 5
+            Console.WriteLine(myArray(i))
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
