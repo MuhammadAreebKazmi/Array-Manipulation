9345d713b0aa7cc7adacafbab66ecfa383c8e067
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-11-28 14:23:46 +0500

    Basic operations for 2D array are observed here.

---
+++ 2D Array Basic Ops.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myArray(5, 4) As String
+        Dim column As Integer = 0
+        Dim row As Integer = 0
+
+        'Initialisation of Array
+        For row = 1 To 5
+            For column = 1 To 4
+                myArray(row, column) = ""
+            Next
+        Next
+
+        ' population of 2D Array
+        For row = 1 To 5
+            For column = 1 To 4
+                Console.Write("input: ")
+                myArray(row, column) = Console.ReadLine
+            Next
+        Next
+
+        ' Output
+        For row = 1 To 5
+            For column = 1 To 4
+                Console.Write(myArray(row, column))
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
